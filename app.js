43946ae95d0d98dd27bf3fad93a0fc79086e303b
Riazul Islam Suddha <suddha6528@gmail.com>
2024-09-24 04:22:27 +0600

    midway-done

--- app.js
+++ app.js
@@ -16,13 +16,24 @@ donateButton.addEventListener("click", function () {
         alert('You donot Have Sufficient Money For Donate!');
         return;
     }
+
     
     document.getElementById("account-balance").innerText = newBalance;
+    const now = new Date()
+    const div = document.createElement('div')
+    div.innerHTML = `
+   <div class="border rounded-xl"> <h3 class="font-semibold text-2xl text-center mb-12">${donateAmount} Taka is Donated for famine-2024 at Feni, Bangladesh</h3>
+   <p class="text-center"> Date : ${now}</p>
+   </div>
+    `
+    
+    document.getElementById("history-container").appendChild(div);
+
 
     const noakhali = parseFloat(document.getElementById("noakhali-balance").innerText);
 
     const noakhaliNewBalance = donateAmount + noakhali;
-
+    
     document.getElementById("noakhali-balance").innerText = noakhaliNewBalance
     
     
